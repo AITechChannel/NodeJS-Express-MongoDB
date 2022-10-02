6761a08e5054e5b9e1bf9df5a6e3e9066d3d87bc
Tuan Anh Doan <dta.bk13@gmail.com>
2022-10-02 20:25:15 +0700

    edit port to PORT

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ import mongoose from "mongoose";
 const app = express();
 const URI =
   "mongodb+srv://admin:oC7zw4GYl8euaFfH@cluster0.d2b7x.mongodb.net/data?retryWrites=true&w=majority";
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true, limit: "30mb" }));
